1fa1ee28c1c3557ac80a8a9743a234ea1cebb124
OleksiiYesin <86779533+OleksiiYesin@users.noreply.github.com>
2022-04-27 23:17:29 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+apt-get update
+apt-get install curl -y
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/v1.20.5/bin/linux/amd64/kubectl"  
+chmod u+x ./kubectl
+./kubectl apply -f react.yml -n jenkins
 
